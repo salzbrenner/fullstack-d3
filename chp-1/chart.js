6062d0b1e0cba67b05af4237b1e88c3af1231cda
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2021-08-17 07:58:29 -0700

    start

---
+++ chp-1/chart.js
@@ -0,0 +1,57 @@
+(() => {
+    const dateParser = d3.timeParse('%Y-%m-%d');
+    const yAccessor = d => d.temperatureMax;
+    const xAccessor  = d => dateParser(d.date);
+    const lineGenerator = d3.line()
+
+    let dimensions = {
+        width: window.innerWidth * 0.9, 
+        height: 400,
+        margin: {
+              top: 15,
+              right: 15,
+              bottom: 40,
+              left: 60,
+        }, 
+    }
+    dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+    dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+    
+    const wrapper = d3.select('#wrapper')
+        .append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height);
+    
+    const bounds = wrapper.append('g')
+        .style('transform', `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`);
+    
+    async function drawLineChart() {
+        const dataset = await d3.json('./my_weather_data.json');
+        const yScale = d3.scaleLinear()
+            .domain(d3.extent(dataset, yAccessor))
+            .range([dimensions.boundedHeight, 0])
+        
+        const xScale = d3.scaleTime() 
+            .domain(d3.extent(dataset, xAccessor)) 
+            .range([0, dimensions.boundedWidth])
+
+        const freezingTemperaturePlacement = yScale(32) 
+        const freezingTemperatures = bounds.append("rect")
+            .attr("x", 0)
+            .attr("width", dimensions.boundedWidth) 
+            .attr("y", freezingTemperaturePlacement) 
+            .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
+            .attr("fill", "#e0f3f3")
+
+        const lineGenerator = d3.line() 
+            .x(d => xScale(xAccessor(d))) 
+            .y(d => yScale(yAccessor(d)))
+
+        const line = bounds.append("path")
+            .datum(dataset)
+            .attr("d", lineGenerator)
+    }
+    
+    drawLineChart();
+})();
+
